85a4620ee07d0e99abd4917ce9fd83cf2f0da848
Petr Valenta <petr.valenta.92@gmail.com>
2024-02-22 08:41:11 +0100

    Add key and openSpots parametrs

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
   const cardElements = data.map( card => {
     return (
       <Card 
+          key = { card.id }
           coverImg = { `/airbnb-clone/src/images/${card.coverImg}` }
           rating = { card.stats.rating }
           reviewCount = { card.stats.reviewCount }
           location = { card.location }
           title = { card.title }
           price = { card.price }
+          openSpots = { card.openSpots }
       />
     )  
   })
